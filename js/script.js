871d2288435bcd4d5be791da7bd9d1dcfed43bf0
Bartek Miastkowski <bartosz1miastkowski@gmail.com>
2023-07-27 20:29:31 +0200

    remove active class from articles

--- js/script.js
+++ js/script.js
@@ -2,10 +2,18 @@
 
 function titleClickHandler(e){
     /* remove class 'active' from all article links  */
+    const activeLinks = document.querySelectorAll('.titles a.active');
+    for(let activeLink of activeLinks){
+        activeLink.classList.remove('active');
+    }
 
   /* add class 'active' to the clicked link */
 
-  /* remove class 'active' from all articles */
+    /* remove class 'active' from all articles */
+    const activeArticles = document.querySelectorAll('.post.active');
+    for(let activeArticle of activeArticles){
+        activeArticle.classList.remove('active');
+    }
 
   /* get 'href' attribute from the clicked link */
 
